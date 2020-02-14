9a26f8bf430a6e72ca9cd177f6146406f8c288b5
sunilsharma8879 <55200091+sunilsharma8879@users.noreply.github.com>
2020-02-14 14:36:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ for($i=0;$i<count($arr);$i++)
 {
     for($j=$i+1;$j<count($arr);$j++)
     {
-        if($arr[$i] > $arr[$j])
+        if($arr[$i] < $arr[$j])
         {
             $temp = $arr[$j];
             $arr[$j] = $arr[$i];
